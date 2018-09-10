0bb265c7048dd4ff065028a4298a0bffda83d7df
Daler Asrorov <asrorids@gmail.com>
2018-09-10 03:22:14 -0700

    Working configs of webpack, react, and typescript

---
+++ index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import ComponentVsPureComponent from './ComponentVsPureComponent';
+
+ReactDOM.render(<ComponentVsPureComponent />, document.getElementById('app'));
